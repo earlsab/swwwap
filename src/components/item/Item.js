568770087ae9d2a524d25383fe346912ff9720cf
earlsab <62688314+earlsab@users.noreply.github.com>
2024-05-23 18:10:38 +0800

    Working Image Upload

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -1,21 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./Item.css";
+import { CldImage } from "next-cloudinary";
 
-const Item = ({ imageUrl, ownerName, title, price, priceDetail, quality, content }) => {
-
+const Item = ({
+  imageUrl,
+  ownerName,
+  title,
+  price,
+  priceDetail,
+  quality,
+  content,
+}) => {
   return (
     <button className="item-container">
-      <img src={imageUrl} alt={title} className="item-image" />
+      {/* TODO: Auto adjust width and height based on image */}
+      <CldImage
+        src={imageUrl}
+        className="item-image"
+        alt={title}
+        width={100}
+        height={100}
+      ></CldImage>
       <div className="item-details">
-
         <p className="item-owner">{ownerName}</p>
         <h2 className="item-title">{title}</h2>
         <p className="item-price">PHP {price}</p>
         <p className="item-price-detail">• {priceDetail}</p>
         <p className="item-quality">• {quality}</p>
         <p className="item-content">• {content}</p>
-        
       </div>
     </button>
   );
