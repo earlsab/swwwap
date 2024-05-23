e9b5fb5302dfcd4034a93226415f585771c063f8
earlsab <62688314+earlsab@users.noreply.github.com>
2024-05-23 20:47:39 +0800

    More data fields

--- src/app/api/protected/data/addItem/route.js
+++ src/app/api/protected/data/addItem/route.js
@@ -1,5 +1,4 @@
 import connectDB from "@/lib/connectDB";
-import { items } from "@/lib/data";
 import Item from "@/model/Item";
 import { withApiAuthRequired, getSession } from "@auth0/nextjs-auth0";
 import { NextResponse } from "next/server";
@@ -13,17 +12,29 @@ export const POST = withApiAuthRequired(async function addItem(req) {
     return;
   }
   await connectDB();
-
-  const res = await req.json(); // res now contains body
+  const res = await req.json();
   console.log(res);
-  const { title, description, price, imageUrl } = res;
+  const {
+    title,
+    description,
+    price,
+    imageUrl,
+    brand,
+    yearsOfOwnership,
+    rfs,
+    itemStatus,
+  } = res;
 
   const item = new Item({
     owner: user.email,
     title: title,
     description: description,
-    price: price,
+    brand: brand,
+    yearsOfOwnership: yearsOfOwnership,
+    rfs: rfs,
     imageUrl: imageUrl,
+    price: price,
+    itemStatus: itemStatus,
   });
   const itemtoSave = await item.save();
   return NextResponse.json({ protected: itemtoSave }, res);
