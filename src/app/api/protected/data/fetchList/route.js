581acc5b8b8a4f4f82342c91d7cb7ef3b061f34a
earlsab <62688314+earlsab@users.noreply.github.com>
2024-05-24 16:22:48 +0800

    working filters!!!

--- src/app/api/protected/data/fetchList/route.js
+++ src/app/api/protected/data/fetchList/route.js
@@ -6,9 +6,31 @@ import { NextResponse } from "next/server";
 // import connectDB from "src/lib/connectDB.js";
 
 export const GET = withApiAuthRequired(async function fetchItems(req) {
+  let items;
   const res = new NextResponse();
-  // const { user } = await getSession(req, res);
   await connectDB();
-  const items = await Item.find({});
+  const { searchParams } = new URL(req.url);
+
+  const filterByBrand = searchParams.get("filterByBrand");
+  if (filterByBrand) {
+    items = await Item.find({ brand: filterByBrand }).exec();
+  }
+
+  const filterByPrice = parseInt(searchParams.get("filterByPrice"));
+  console.log(filterByPrice);
+  const range = filterByPrice * 0.25; // get 10 percent
+
+  const upperLimit = filterByPrice + range;
+  const lowerLimit = filterByPrice - range;
+  console.log(filterByPrice, upperLimit, lowerLimit);
+  if (filterByPrice) {
+    items = await Item.find({
+      price: { $lte: upperLimit, $gte: lowerLimit },
+    }).exec();
+  }
+
+  if (!filterByBrand && !filterByPrice) {
+    items = await Item.find({});
+  }
   return NextResponse.json({ protected: items }, res);
 });
